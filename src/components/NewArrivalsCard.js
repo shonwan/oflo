d20440b947a581806828c2d0c88a181cb11e82c6
shonwan <151938713+shonwan@users.noreply.github.com>
2024-01-29 15:18:08 +0800

    Add files via upload

---
+++ src/components/NewArrivalsCard.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View,Image } from 'react-native';
+import React from 'react';
+
+const NewArrivalsCard = ({title,brand,price,image}) => {
+  return (
+    <View className="max-w-[150px] justify-center items-center overflow-hidden mr-6">
+      <Image source={{uri:image}} className="rounded-xl  h-36 w-32" />
+      <View className="mt-2 justify-center items-center">
+      <Text className="font-bold">{title}</Text>
+      <Text className="text-xs">{brand}</Text>
+      <Text className="font-extrabold">Php {price}</Text>
+      </View>
+    </View>
+  )
+}
+
+export default NewArrivalsCard
+
+const styles = StyleSheet.create({})
